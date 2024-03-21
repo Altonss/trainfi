a09bed35ecdcb3fdf7bc7d617e1bdea9ec4b0b32
Altonss <66519591+Altonss@users.noreply.github.com>
2024-03-21 18:47:50 +0100

    Update ouifi.js
    Make eslint happy

--- ouifi.js
+++ ouifi.js
@@ -1,42 +1,41 @@
 import GLib from 'gi://GLib';
-import St from 'gi://St';
-import Soup from "gi://Soup";
+import Soup from 'gi://Soup';
 
 
 class Stop {
-    constructor(label, theoric_date, real_date, is_delayed, is_created, is_diversion, is_removed) {
+    constructor(label, theoricDate, realDate, isDelayed, isCreated, isDiversion, isRemoved) {
         this.label = label;
-        this.theoric_date = new Date(theoric_date);
-        this.real_date = new Date(real_date);
-        this.is_delayed = is_delayed;
-        this.is_created = is_created;
-        this.is_diversion = is_diversion;
-        this.is_removed = is_removed;
+        this.theoricDate = new Date(theoricDate);
+        this.realDate = new Date(realDate);
+        this.isDelayed = isDelayed;
+        this.isCreated = isCreated;
+        this.isDiversion = isDiversion;
+        this.isRemoved = isRemoved;
     }
 
     inThePast() {
         const now = new Date();
-        return now > new Date(this.real_date.getTime() + 5 * 60 * 1000);
+        return now > new Date(this.realDate.getTime() + 5 * 60 * 1000);
     }
 
     pangoTheoric() {
-        const localTime = this.theoric_date.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
-        return this.is_delayed ? ` <span foreground="red"><s>${localTime}</s></span>` : ` ‎<span foreground="green">${localTime}</span>`;
+        const localTime = this.theoricDate.toLocaleTimeString(undefined, {hour: '2-digit', minute: '2-digit'});
+        return this.isDelayed ? ` <span foreground="red"><s>${localTime}</s></span>` : ` ‎<span foreground="green">${localTime}</span>`;
     }
 
     pangoReal() {
-        const localTime = this.real_date.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
-        return this.is_delayed ? `<span foreground="green">${localTime}</span>` : '';
+        const localTime = this.realDate.toLocaleTimeString(undefined, {hour: '2-digit', minute: '2-digit'});
+        return this.isDelayed ? `<span foreground="green">${localTime}</span>` : '';
     }
 
     pangoFormatedLabel() {
         const [status, color] = this.inThePast()
             ? [' ', 'grey']
-            : this.is_created
+            : this.isCreated
                 ? ['+', 'green']
-                : this.is_removed
+                : this.isRemoved
                     ? ['-', 'red']
-                    : this.is_diversion
+                    : this.isDiversion
                         ? ['~', 'yellow']
                         : ['·', 'white'];
 
@@ -47,14 +46,14 @@ class Stop {
 class Trip {
     constructor(stops) {
         this.stops = stops.map(
-            ({ label, theoricDate, realDate, isDelayed, isCreated, isDiversion, isRemoved }) =>
+            ({label, theoricDate, realDate, isDelayed, isCreated, isDiversion, isRemoved}) =>
                 new Stop(label, theoricDate, realDate, isDelayed, isCreated, isDiversion, isRemoved)
         );
     }
 }
 
 export function connected() {
-    const [success, out] = GLib.spawn_command_line_sync('nmcli connection show --active');
+    const [, out] = GLib.spawn_command_line_sync('nmcli connection show --active');
     const wifiInfo = out.toString();
     return wifiInfo.includes('_SNCF_WIFI_INOUI');
 }
@@ -67,9 +66,9 @@ export async function speed() {
       _httpSession.send_and_read_async(_message, GLib.PRIORITY_DEFAULT, null, (sess, result) => {
           try {
               let response = sess.send_and_read_finish(result).get_data();
-              let speed_ms = JSON.parse(response)["speed"];
-              let speed_val = Math.round(speed_ms * 3.6);
-              resolve(speed_val);
+              let speedMs = JSON.parse(response)["speed"];
+              let speedVal = Math.round(speedMs * 3.6);
+              resolve(speedVal);
           } catch (e) {
               sess.abort();
               reject(e);
